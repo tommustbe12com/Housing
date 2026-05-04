069d7be077fd6a7bfcc44d146f65d5b687e0345f
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2026-05-03 22:30:47 -0700

    Initial commit

---
+++ src/main/java/com/tommustbe12/housing/Housing.java
@@ -0,0 +1,17 @@
+package com.tommustbe12.housing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Housing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
